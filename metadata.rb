58fb1de058e8ba653ea5545a88f9c5cb9a9e02ab
Panggi Libersa Jasri Akadol <panggilja@gmail.com>
2014-06-12 17:05:04 +0700

    v0.0.2

--- metadata.rb
+++ metadata.rb
@@ -2,7 +2,7 @@ name              "nginx-centos"
 maintainer        "Panggi Libersa Jasri Akadol"
 maintainer_email  "panggi@me.com"
 description       "Installs the nginx web server for Centos 6"
-version           "0.0.1"
+version           "0.0.2"
 
 recipe "nginx-centos", "nginx server"
 
